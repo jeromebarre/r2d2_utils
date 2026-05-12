032cac0dafe939f859e64096cc79c84b8bc876a3
jeromebarre <jerome.e.barre@gmail.com>
2026-05-12 18:57:02 -0400

    fix

--- list_r2d2_exps_v3.py
+++ list_r2d2_exps_v3.py
@@ -11,7 +11,12 @@ R2D2_DB_ROOTS = {
     'discover-mil-gnu':   '/css/jcsda/s2127/r2d2-experiments-nccs/',
     'discover-gmao-intel': '/discover/nobackup/projects/gmao/swell/r2d2-experiments/',
 }
+DATA_STORES = {
+    'discover-mil-gnu':   'r2d2-experiments-nccs',
+    'discover-gmao-intel': 'r2d2-experiments',
+}
 r2d2_db_root = R2D2_DB_ROOTS[compute_host]
+data_store = DATA_STORES[compute_host]
 
 
 def get_experiment_file_info(model, experiment, item):
@@ -19,9 +24,9 @@ def get_experiment_file_info(model, experiment, item):
     r2d2_db = r2d2_db_root
 
     if item == "feedback":
-        files = r2d2.search(experiment=experiment, item=item, include_item_index=True)
+        files = r2d2.search(experiment=experiment, item=item, data_store=data_store, include_item_index=True)
     else:
-        files = r2d2.search(model=model, experiment=experiment, item=item, include_item_index=True)
+        files = r2d2.search(model=model, experiment=experiment, item=item, data_store=data_store, include_item_index=True)
 
     total_size = 0
     for f in files:
